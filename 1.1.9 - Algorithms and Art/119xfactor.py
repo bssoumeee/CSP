0dea5a9f2c204f96249166dba4d8ba79ec7ec021
bssoumeee <311526@shakopeeschools.org>
2025-10-01 09:37:21 -0500

    Update 119xfactor.py

--- 1.1.9 - Algorithms and Art/119xfactor.py
+++ 1.1.9 - Algorithms and Art/119xfactor.py
@@ -1,6 +1,6 @@
 import turtle as trtl
 #trtl shapes for animals
-trtl.addshape("trotters"((-4,0),(-2,-4),(0,0),(2,-4),(4,0)))
+#trtl.addshape("trotters"((-4,0),(-2,-4),(0,0),(2,-4),(4,0)))
 #animal_parts = ["trotters"]
 #Choose animal
 animals = ["Pig", "Rat", "Chicken"]
@@ -9,8 +9,48 @@ animals = ["Pig", "Rat", "Chicken"]
 animal_chosen = input("Pig, Rat, or Chicken?")
 
 if animal_chosen == "Pig":
-  t = trtl.Turtle("trotters")
-  t.forward(50)
+ #Body
+ trtl.penup()
+ trtl.goto(0,-80)
+ trtl.pendown()
+ trtl.circle(80)
+ #Head
+ trtl.penup()
+ trtl.goto(0,70)
+ trtl.pendown()
+ trtl.circle(65)
+ #Ears
+ trtl.penup()
+ trtl.goto(35,190)
+ trtl.pendown()
+ trtl.circle(15)
+ trtl.penup()
+ trtl.goto(-35, 190)
+ trtl.pendown()
+ trtl.circle(15)
+ #Nose
+ trtl.penup()
+ trtl.goto(0,105)
+ trtl.pendown()
+ trtl.circle(15)
+ trtl.penup()
+ trtl.goto(-5,115)
+ trtl.pendown()
+ trtl.circle(5)
+ trtl.penup()
+ trtl.goto(5,115)
+ trtl.pendown()
+ trtl.circle(5)
+ #Eyes
+ trtl.penup()
+ trtl.goto(-25,160)
+ trtl.pendown()
+ trtl.circle(8)
+ 
+ 
+
+ 
+ 
 
 
 
