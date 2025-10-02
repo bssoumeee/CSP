0d2fab853b656587a9bfd7f99fa3d4cf9831797b
bssoumeee <311526@shakopeeschools.org>
2025-10-02 09:09:39 -0500

    Update 119xfactor.py

--- 1.1.9 - Algorithms and Art/119xfactor.py
+++ 1.1.9 - Algorithms and Art/119xfactor.py
@@ -54,16 +54,20 @@ if animal_chosen == "Pig":
  trtl.pendown()
  trtl.circle(8)
  #Tail
-# for i in range(60):
-  #trtl.forward(50)
-  #trtl.right(60)
+ trtl.penup()
+ trtl.goto(70,-35)
+ trtl.pendown()
+ trtl.pencolor("pink")
+ for i in range(12):
+  trtl.forward(8 + i)
+  trtl.left(40)
 
 if animal_chosen == "Chicken":
  #Body
  trtl.speed(100)
  trtl.penup()
  trtl.goto(0,-80)
- #trtl.color("yellow")
+ trtl.color("yellow")
  trtl.pendown()
  trtl.circle(80)
  #Head
@@ -77,7 +81,7 @@ if animal_chosen == "Chicken":
  trtl.goto(-25,135)
  trtl.pendown()
  trtl.circle(12)
-#trtl.pencolor("black")
+ trtl.pencolor("black")
  trtl.penup()
  trtl.goto(-25, 143)
  trtl.pendown()
@@ -89,7 +93,7 @@ if animal_chosen == "Chicken":
  trtl.penup()
  trtl.goto(25,135)
  trtl.pendown()
- #trtl.pencolor("yellow")
+ trtl.pencolor("yellow")
  trtl.circle(12)
  trtl.pencolor("black")
  trtl.penup()
@@ -106,18 +110,41 @@ if animal_chosen == "Chicken":
  trtl.circle(9)
  
 #wings
-trtl.penup()
-trtl.goto(77,20)
-trtl.pendown()
-trtl.circle(28, 258)
+ trtl.pencolor("yellow")
+ trtl.penup()
+ trtl.goto(77,20)
+ trtl.pendown()
+ trtl.circle(28, 258)
  
-trtl.penup()
-trtl.goto(-53,60)
-trtl.pendown()
-trtl.setheading(120)
-trtl.circle(28, 260)
+ trtl.penup()
+ trtl.goto(-53,60)
+ trtl.pendown()
+ trtl.setheading(120)
+ trtl.circle(28, 260)
+
+#feet
+ trtl.penup()
+ trtl.goto(-33, -115)
+ trtl.pendown()
+ trtl.circle(23)
+ trtl.penup()
+ trtl.goto(42,-117)
+ trtl.pendown()
+ trtl.circle(23)
 
 #Head thingy
+ trtl.penup()
+ trtl.goto(-38,176)
+ trtl.pendown()
+ trtl.circle(19)
+ trtl.penup()
+ trtl.goto(0,189)
+ trtl.pendown()
+ trtl.circle(19)
+ trtl.penup()
+ trtl.goto(38,182)
+ trtl.pendown()
+ trtl.circle(19)
 
 
 
