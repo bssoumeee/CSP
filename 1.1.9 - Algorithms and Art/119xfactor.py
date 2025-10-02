1a9dd907d13690f30209a536107bf0761c841ccf
bssoumeee <311526@shakopeeschools.org>
2025-10-01 23:44:55 -0500

    Update 119xfactor.py
    added chicken almst finised it just need feet and glubber thingy

--- 1.1.9 - Algorithms and Art/119xfactor.py
+++ 1.1.9 - Algorithms and Art/119xfactor.py
@@ -6,10 +6,12 @@ import turtle as trtl
 animals = ["Pig", "Rat", "Chicken"]
 
 #ask what animal they want
-animal_chosen = input("Pig, Rat, or Chicken?")
+animal_chosen = input("Pig, or Chicken?")
 
 if animal_chosen == "Pig":
  #Body
+ trtl.speed(100)
+ trtl.pencolor("pink")
  trtl.penup()
  trtl.goto(0,-80)
  trtl.pendown()
@@ -42,15 +44,81 @@ if animal_chosen == "Pig":
  trtl.pendown()
  trtl.circle(5)
  #Eyes
+ trtl.color("black")
  trtl.penup()
- trtl.goto(-25,160)
+ trtl.goto(-25,155)
  trtl.pendown()
  trtl.circle(8)
- 
- 
+ trtl.penup()
+ trtl.goto(25,155)
+ trtl.pendown()
+ trtl.circle(8)
+ #Tail
+# for i in range(60):
+  #trtl.forward(50)
+  #trtl.right(60)
 
+if animal_chosen == "Chicken":
+ #Body
+ trtl.speed(100)
+ trtl.penup()
+ trtl.goto(0,-80)
+ #trtl.color("yellow")
+ trtl.pendown()
+ trtl.circle(80)
+ #Head
+ trtl.penup()
+ trtl.goto(0,70)
+ trtl.pendown()
+ trtl.circle(60)
+ #eyes and nose
+
+ trtl.penup()
+ trtl.goto(-25,135)
+ trtl.pendown()
+ trtl.circle(12)
+#trtl.pencolor("black")
+ trtl.penup()
+ trtl.goto(-25, 143)
+ trtl.pendown()
+ trtl.fillcolor("black")
+ trtl.begin_fill()
+ trtl.circle(4)
+ trtl.end_fill()
  
+ trtl.penup()
+ trtl.goto(25,135)
+ trtl.pendown()
+ #trtl.pencolor("yellow")
+ trtl.circle(12)
+ trtl.pencolor("black")
+ trtl.penup()
+ trtl.goto(25, 143)
+ trtl.pendown()
+ trtl.fillcolor("black")
+ trtl.begin_fill()
+ trtl.circle(4)
+ trtl.end_fill()
+
+ trtl.penup()
+ trtl.goto(0,120)
+ trtl.pendown()
+ trtl.circle(9)
  
+#wings
+trtl.penup()
+trtl.goto(77,20)
+trtl.pendown()
+trtl.circle(28, 258)
+ 
+trtl.penup()
+trtl.goto(-53,60)
+trtl.pendown()
+trtl.setheading(120)
+trtl.circle(28, 260)
+
+#Head thingy
+
 
 
 
