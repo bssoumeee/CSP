11f8a7efd7e215eeeb29a4118de5743dbbd553b7
bssoumeee <311526@shakopeeschools.org>
2025-10-28 09:43:53 -0500

    finsihing up the game setup

--- 1.2.5 - Shall We Play a Game/125 flappybird maliya.py
+++ 1.2.5 - Shall We Play a Game/125 flappybird maliya.py
@@ -3,22 +3,49 @@
 # TODO Code the key presses for the bird to flap
 # TODO End screen with scores if you die
 import turtle as trtl
-#Setup
-trtl.addshape("Bird", ((0,3), (2,3), (3,2), (3,1), (4,1), (5,0), (4,-1), (3,-1), (2,-2), (0,-3), (-2,-3), (-4,-2), (-5,-1), (-5,1), (-4,2), (-3,3)))
-Burd = trtl.Turtle(shape="Bird")
-Burd.left(90)
-Burd.shapesize(5)
-Burd.color("yellow")
+wn = trtl.Screen()
+wn.bgpic("flappyburd.gif")
 
+#Setup
 start_button = trtl.Turtle()
 start_button.shape("square")
 start_button.color("Yellow")
 start_button.penup()
-start_button.goto(0,0)
-start_button.write("Click to start", align="center", font=("Arial", 16, "bold"))
+start_button.goto(0,-35)
+
+
+trtl.addshape("Bird", ((0,3), (2,3), (3,2), (3,1), (4,1), (5,0), (4,-1), (3,-1), (2,-2), (0,-3), (-2,-3), (-4,-2), (-5,-1), (-5,1), (-4,2), (-3,3)))
+Burd = trtl.Turtle(shape="Bird")
+Burd.hideturtle()
+Burd.penup()
+Burd.goto(-180,0)
+Burd.left(90)
+Burd.shapesize(5)
+Burd.color("yellow")
+
+def fall(x,y):
+    Burd.forward(4)
+    Burd.speed(1)
+
+def game_start(x,y):
+    start_button.hideturtle()
+    Burd.showturtle()
+  
+
+while game_start == True:
+    fall
 
 
 
 
-wn = trtl.Screen()
+start_button.onclick(game_start)
+
+
+
+
+
+
+
+
 wn.mainloop()
+
